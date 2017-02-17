b6446ca142ec3f39a00f5016c7fb76a6d4b60681
Tokery <kevincott97@gmail.com>
2017-02-16 22:40:00 -0500

    Up to video 3.1

--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 ### Running babel
 node_modules\.bin\babel-node server.js
+
+./node_modules/.bin/babel-node server.js
--- server.js
+++ server.js
@@ -1,3 +1,28 @@
-import config, { nodeEnv } from './config';
+import config from './config';
+import apiRouter from './api';
 
-console.log (config, nodeEnv);
+import express from 'express';
+const server = express();
+
+server.set('view engine', 'ejs');
+//server.set('views', path.join(__dirname, 'views'));
+
+server.get('/', (req, res) => {
+    res.render('index', {
+        content: 'Hello Express and EJS!'
+    });
+});
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+//import fs from 'fs';
+// server.get('/about.html', (req, res) => {
+//     fs.readFile('./about.html', (err, data) => {
+//         res.send(data.toString());
+//     });
+// });
+
+server.listen(config.port, () => {
+    console.info('Express listening on port ', config.port);
+});
